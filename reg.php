3179c94c32b60245916fa157da89c3fa5af8dc4a
PhucDZ <tdnnguyenhuuphuc@gmail.com>
2023-04-27 00:03:05 +0700

    update

---
+++ reg.php
@@ -0,0 +1,33 @@
+<?php
+require 'db/conect.php';
+if(isset($_POST['btn-reg'])){
+    echo "da submit";
+    // echo "<pre>";
+    // print_r($_POST);
+    $fullname = $_POST['fullname']; 
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+    if(!empty($username)&&!empty($fullname)&&!empty($email)&&!empty($phone)&&!empty($password)){
+    //  inserrt dữ liệu
+    // echo "<pre>";
+    // print_r($_POST);
+
+    $sql = "INSERT INTO `tbl_users` (`fullname`,`email`,`username`,`phone`,`password`) VALUE('$fullname','$email','$username','$phone','$password')";
+    if($conn->query($sql)===TRUE){
+        echo "Lưu dữ liệu thành công";
+    }
+    else{
+        echo "lỗi {$sql}".$conn->error;
+    }
+    }
+    else{
+        echo "Bạn cần nhập đầy đủ trước khi đăng ký";
+    }
+}
+// echo "<pre>";
+// print_r($_POST);
+?>
+<a href="index.php">Quay lại</a>
